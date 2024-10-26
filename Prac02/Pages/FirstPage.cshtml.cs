33e222ad7287824ceab0985e93352f871aee17ad
H.D.Dakshina Prasad <dakshinaprasad345@gmail.com>
2024-10-26 17:37:56 +0530

    The continous Project

---
+++ Prac02/Pages/FirstPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prac02.Pages
+{
+    public class FirstPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
